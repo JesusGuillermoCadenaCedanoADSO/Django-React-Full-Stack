26aa5216363bed80ab8924a1f230d81e252f8cc3
JesusGuillermoCadenaCedanoADSO <cadena.jesus@gmail.com>
2025-04-04 18:59:59 -0500

    api.js modificado

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/django-react-tutorial/backend/v1";
+const apiUrl = "https://ea02c024-7e9b-42de-8638-87e7d9d5b191-dev.e1-us-east-azure.choreoapis.dev/django-react-tutorial/backend/v1";
 
 const api = axios.create({
    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
